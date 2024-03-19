7edb6c723cc72cabfae8180d4a5a839193e48184
sahilnamdeo2002 <117310162+sahilnamdeo2002@users.noreply.github.com>
2024-03-19 15:59:28 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+
+var typed= new typed(".text",{
+    Strings:["Frontend Developer","Python creator","React js"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
+function myfunction(){
+    window.open("certificate.html")
+    document.getElementById(compiler).style.display="block";
+} 
+function myfunction1(){
+    window.open("resume.html")
+    document.getElementById(compiler).style.display="block";
+}
