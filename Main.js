93aa02220b7f313cf05b21604e19d03a4069839e
sahil namdeo <https://github.com/sahilnamdeo2002/backend-project.git>
2024-09-02 23:31:24 +0530

    new updates

---
+++ Main.js
@@ -0,0 +1,24 @@
+
+var typed= new typed(".text",{
+    Strings:["Frontend Developer","Python creator","React js"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
+function myfunction(){
+    window.open("certificate.html")
+    document.getElementById(compiler).style.display="block";
+} 
+function myfunction1(){
+    window.open("resume.html")
+    document.getElementById(compiler).style.display="block";
+}
+function myfunction2(){
+    window.open("contact.html")
+    document.getElementById(compiler).style.display="block";
+}
+function myfunction3(){
+    window.open("about.html")
+    document.getElementById(compiler).style.display="block";
+}
---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed= new typed(".text",{
+    Strings:["Frontend Developer","Python creator","React js"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
